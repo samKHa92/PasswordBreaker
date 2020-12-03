0687cd3d873052e904887582c4ae28d5208efa33
kiu_samkharadze.luka <samkharadze.luka@kiu.edu.ge>
2020-12-03 22:47:27 +0400

    hgv

--- src/fop/w8iter/PasswordIterator.java
+++ src/fop/w8iter/PasswordIterator.java
@@ -2,20 +2,25 @@ package fop.w8iter;
 
 import java.util.Iterator;
 
-public class PasswordIterator implements Iterator<String> {
+public class PasswordIterator implements Iterator<String> 
+{
 
-    public PasswordIterator(int passwordLength) {
-        // TODO
+    public PasswordIterator(int passwordLength) 
+    {
+    	if(passwordLength < 1 || passwordLength > 9)
+    		Util.badArgument("Password length is not appropriate");
     }
 
-    public boolean hasNext() {
-        // TODO
-        return false;
+    public boolean hasNext() 
+    {
+        return (this.next() != null);
     }
 
-    public String next() {
-        // TODO
-        return null;
+    public String next() 
+    {
+    	if(this.hasNext())
+    		return this.next();
+    	return null;
     }
 
 }
--- src/fop/w8iter/Range.java
+++ src/fop/w8iter/Range.java
@@ -1,1 +1,73 @@
 package fop.w8iter;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Range implements Iterable<Integer>
+{
+	private final int begin;
+	private final int end;
+	private final int stride;
+	
+	private static int nextValue;
+	
+	public Range(int begin, int end, int stride)
+	{
+		this.begin = begin;
+		this.end = end;
+		this.stride = stride;
+		nextValue = begin;
+		if(stride <= 0)
+			Util.badArgument("Stride should be positive.");
+	}
+	
+	public Range(int begin, int end)
+	{
+		this.begin = begin;
+		this.end = end;
+		this.stride = 1;
+	}
+	
+	public Iterator<Integer> iterator()
+	{
+		ArrayList <Integer> list = new ArrayList<>();
+		Iterator<Integer> iter = null;
+		if(end > begin)
+		{
+			for(int i = this.begin; (i+stride) <= this.end; i += stride)
+				list.add(i);
+			iter = list.iterator();
+		}
+		else if(begin > end)
+		{
+			for(int i = this.end; (i-stride) >= this.begin; i -= stride)
+				list.add(i);
+			iter = list.iterator();
+		}
+		else 
+		{
+			list.add(this.begin);
+			iter = list.iterator();
+		}
+		
+		return iter;
+	}
+	
+	public Integer next() 
+	{
+	    if(hasNext() == false) 
+	      Util.noSuchElement("There is no more elements.");
+	    int ret = nextValue + this.stride;
+	    nextValue += this.stride;
+	    return Integer.valueOf(ret);
+	}
+	
+	public boolean hasNext()
+	{
+		if(this.next() <= this.end) 
+			return true;
+		return false;
+	}
+	
+	
+}
